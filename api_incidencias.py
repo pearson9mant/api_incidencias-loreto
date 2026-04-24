5e8ceab8972eed19c21ec7bceb944d1441b5d3d1
pearson9mant <jalmedac@abatoliba.edu>
2026-04-24 19:34:55 +0200

    Update api_incidencias.py

--- api_incidencias.py
+++ api_incidencias.py
@@ -11,10 +11,7 @@ app = FastAPI(title="API Incidencias")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://pearson9mant.github.io",
-        "https://almedainstalacio-commits.github.io",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
